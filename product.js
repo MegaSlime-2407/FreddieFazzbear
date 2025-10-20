ff9fc68d5346fe092b4519017dcdb5b71f4cb214
Wolfenste1n <wolfenstrun@gmail.com>
2025-10-21 00:45:11 +0500

    Task 2 part 2

--- product.js
+++ product.js
@@ -1,3 +1,4 @@
+// Task 2 Part 1 
 const btn = document.getElementById('btn-show-time');
 const out = document.getElementById('time-output');
 
@@ -5,3 +6,22 @@ btn.addEventListener('click', () => {
   const now = new Date().toLocaleTimeString();
   out.textContent = "Current time: " + now;
 });
+
+//Task 2 Part 2
+const nav = document.querySelector("nav");
+const links = nav.querySelectorAll("a");
+let index = 0;
+
+nav.addEventListener("keydown", function(e) { //e - object(event)
+    if (e.key === "ArrowRight") {
+      index++;
+      if (index >= links.length) index = 0;
+      links[index].focus();
+    }
+  
+    if (e.key === "ArrowLeft") {
+      index--;
+      if (index < 0) index = links.length - 1;
+      links[index].focus();
+    }
+  });
